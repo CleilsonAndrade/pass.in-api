aebfa725ed2687d1ed2985d0361d1a4fb09f0dd6
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-04-03 23:18:49 -0300

    🔎 ref: create event resource

---
+++ src/routes/create-event.ts
@@ -0,0 +1,52 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { prisma } from "../lib/prisma";
+import { generateSlug } from "../utils/generate-slug";
+
+export async function createEvent(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .post('/events',
+      {
+        schema: {
+          body: z.object({
+            title: z.string().min(4),
+            details: z.string().nullable(),
+            maximumAttendees: z.number().int().positive().nullable(),
+          }),
+          response: {
+            201: z.object({
+              eventId: z.string().uuid()
+            })
+          }
+        }
+      },
+      async (request, response) => {
+        const { title, details, maximumAttendees } = request.body
+
+        const slug = generateSlug(title)
+
+        const eventWithSameSlug = await prisma.event.findUnique({
+          where: {
+            slug,
+          }
+        })
+
+        if (eventWithSameSlug !== null) {
+          throw new Error('Another event with same title already exists')
+        }
+
+        const event = await prisma.event.create({
+          data: {
+            title,
+            details,
+            maximumAttendees,
+            slug,
+          }
+        })
+
+        return response.status(201).send({ eventId: event.id })
+      })
+}
+
--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,13 @@
-import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
-import { z } from 'zod';
-import { generateSlug } from "./utils/generate-slug";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
 
 const app = fastify();
 
-const prisma = new PrismaClient({
-  log: [
-    "query"
-  ]
-});
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-app.post('/events', async (request, response) => {
-  const createEventSchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximumAttendees: z.number().int().positive().nullable(),
-  })
-
-  const { title, details, maximumAttendees } = createEventSchema.parse(request.body)
-
-  const slug = generateSlug(title)
-
-  const eventWithSameSlug = await prisma.event.findUnique({
-    where: {
-      slug,
-    }
-  })
-
-  if (eventWithSameSlug !== null) {
-    throw new Error('Another event with same title already exists')
-  }
-
-  const event = await prisma.event.create({
-    data: {
-      title,
-      details,
-      maximumAttendees,
-      slug,
-    }
-  })
-
-  return response.status(201).send(event.id)
-})
+app.register(createEvent)
 
 app.listen({
   port: 3333,
